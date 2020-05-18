f426071bb08335f0bc1e9737bf033f66df22d4aa
yugasun <yuga_sun@163.com>
2020-05-18 20:45:39 +0800

    feat: update cdn creating logical

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const CONFIGS = {
+  templateUrl:
+    'https://serverless-templates-1300862921.cos.ap-beijing.myqcloud.com/express-demo.zip',
+  region: 'ap-guangzhou',
+  indexPage: 'index.html',
+  errorPage: 'error.html',
+  protocol: 'http',
+  description: 'This is a bucket created by serverless component'
+}
+
+module.exports = CONFIGS
