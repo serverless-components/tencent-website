c96a54a2daef7dde50e0b49fa4016bff5ce64fb4
Avril Li <linxforever@vip.qq.com>
2020-06-29 15:27:42 +0800

    chore: remove process.env variables of test

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -17,11 +17,6 @@ const instanceYaml = {
   }
 }
 
-process.env.SERVERLESS_PLATFORM_VENDOR = 'tencent'
-process.env.TENCENT_SECRET_ID = ''
-process.env.TENCENT_SECRET_KEY = ''
-process.env.SERVERLESS_PLATFORM_STAGE = 'dev'
-
 // get credentials from process.env but need to init empty credentials object
 const credentials = {
   tencent: {}
